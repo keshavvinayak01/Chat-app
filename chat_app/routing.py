14df8b7e0f9d5b09ed8a74831381379de2c99a33
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-29 18:11:50 +0530

    Corrected Type preventing webSocket from running

--- chat_app/routing.py
+++ chat_app/routing.py
@@ -1,5 +1,16 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
+from chat import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+]
 
 application = ProtocolTypeRouter({
-    
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
 })
