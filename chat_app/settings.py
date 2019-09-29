f7886109d1f5b863943cc7c561caf2a0eed0f644
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-29 18:55:28 +0530

    Modified settings

--- chat_app/settings.py
+++ chat_app/settings.py
@@ -74,6 +74,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chat_app.wsgi.application'
 ASGI_APPLICATION = 'chat_app.routing.application'
 
+CHANNEL_LAYERS = {
+    'default' : {
+        'BACKEND' : 'channels_redis.core.RedisChannelLayer',
+        'config' : {
+            'hosts' : [('127.0.0.1', 6379)],
+        }
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
