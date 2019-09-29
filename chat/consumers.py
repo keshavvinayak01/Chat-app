6d9743291b000ea1ad6cdbfa178fab44dfacc7f8
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-09-29 19:10:42 +0530

    Added Asynchronous handling

--- chat/consumers.py
+++ chat/consumers.py
@@ -16,7 +16,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     
     async def disconnect(self, close_code):
         # Leave room group
-        await self.channel_layer.group_discar)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
