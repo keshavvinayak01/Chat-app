6c46c288f69873062235018908c8be1f3533a9ef
Keshav Vinayak <keshavvinayakjha@gmail.com>
2019-10-01 16:18:10 +0530

    Refactoring backend

--- chat_app/routing.py
+++ chat_app/routing.py
@@ -4,7 +4,7 @@ from django.urls import re_path
 from chat import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat$', consumers.ChatConsumer),
 ]
 
 application = ProtocolTypeRouter({
--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,8 +1,6 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
     path('admin/', admin.site.urls)
 ]
